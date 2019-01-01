f251edf2ad48af79ac5f98c173ec8444493e64e5
amaad0 <40160471+amaad0@users.noreply.github.com>
2019-01-02 00:37:41 +0500

    api_invoke has been added

--- website/js/config.js
+++ website/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qj8fvp8b7d.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://5hie47x5hj.execute-api.us-east-1.amazonaws.com/workspace_prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'earl@eeg3.net'
